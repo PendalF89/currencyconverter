9a2aa5f0baeb1825aa0bf9f5593cb248949c201c
Kolya Korobochkin <me@korobochkin.com>
2015-12-11 15:56:25 +0300

    Наброски виджета

---
+++ plugin/source/Widgets/CurrencyTable/Currency_Table.php
@@ -0,0 +1,40 @@
+<?php
+namespace Korobochkin\Currency\Widgets\CurrencyTable;
+
+use Korobochkin\Currency\Plugin;
+
+class Currency_Table extends \WP_Widget {
+
+	public function __construct() {
+		parent::__construct(
+			'currency_table',
+			__( 'Currency Table', Plugin::NAME ),
+			array(
+				'classname' => 'widget_currency_table',
+				'description' => __( 'A table with currency rates.', Plugin::NAME )
+			)
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+
+		/**
+		 * Title
+		 */
+		$title = apply_filters(
+			'widget_title',
+			empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base
+		);
+		if( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+
+		/**
+		 * Table
+		 */
+
+		echo $args['after_widget'];
+	}
+
+}
