ceab06ee24161f53ecafaf35ae47df7da0956998
Kolya Korobochkin <me@korobochkin.com>
2015-12-13 02:03:43 +0300

    Форма настроек виджета

--- plugin/source/Widgets/CurrencyTable/Currency_Table.php
+++ plugin/source/Widgets/CurrencyTable/Currency_Table.php
@@ -47,4 +47,22 @@ class Currency_Table extends \WP_Widget {
 		echo $args['after_widget'];
 	}
 
+	public function form( $instance ) {
+		$title = sanitize_text_field( $instance['title'] );
+		$base_currency = sanitize_text_field( $instance['base_currency'] );
+		$currency_list = sanitize_text_field( $instance['currency_list'] );
+		?>
+
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', Plugin::NAME ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>"></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'base_currency' ); ?>"><?php _e( 'Base currency:', Plugin::NAME ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'base_currency' ); ?>" name="<?php echo $this->get_field_name( 'base_currency' ); ?>" type="text" value="<?php echo esc_attr( $base_currency ); ?>"></p>
+		<p class="description"><?php _e( 'The currency in which will be settled other currencies.', Plugin::NAME ); ?></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'currency_list' ); ?>"><?php _e( 'Currencies list:', Plugin::NAME ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'currency_list' ); ?>" name="<?php echo $this->get_field_name( 'currency_list' ); ?>" type="text" value="<?php echo esc_attr( $currency_list ); ?>"></p>
+		<p class="description"><?php _e( 'The currencies which will be displayed in table. Separate by commas.', Plugin::NAME ); ?></p>
+		<?php
+	}
 }
