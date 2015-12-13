bfe9fadd0782104e7c9a39b0d3d6906ae50b4573
Kolya Korobochkin <me@korobochkin.com>
2015-12-13 23:07:31 +0300

    Сохраняем содержимое виджета и выносим скрипт автодополнения в отдельный файл

--- plugin/source/Admin/Pages/Widgets.php
+++ plugin/source/Admin/Pages/Widgets.php
@@ -1,6 +1,8 @@
 <?php
 namespace Korobochkin\Currency\Admin\Pages;
 
+use Korobochkin\Currency\Plugin;
+
 class Widgets {
 
 	public static function admin_enqueue_scripts( $hook ) {
@@ -8,5 +10,12 @@ class Widgets {
 			return;
 		}
 		wp_enqueue_script( 'jquery-ui-autocomplete' );
+		wp_enqueue_script(
+			'plugin__currency__currency-table__admin',
+			plugin_dir_url( $GLOBALS['CurrencyPlugin']->plugin_path ) . 'source/Widgets/CurrencyTable/admin-page-autocomplete.js',
+			array( 'jquery' ),
+			'0.0.0',
+			true
+		);
 	}
 }
