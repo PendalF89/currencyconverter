615acc4bf0f1b00e2ea5be40976b8523f3fa60cf
Kolya Korobochkin <me@korobochkin.com>
2015-12-16 01:33:24 +0300

    В процессе запиливания генерации таблицы

--- plugin/source/Service/CurrencyTable.php
+++ plugin/source/Service/CurrencyTable.php
@@ -2,6 +2,8 @@
 namespace Korobochkin\Currency\Service;
 
 use HtmlTableGenerator\Table;
+use Korobochkin\Currency\Models\Country;
+use Korobochkin\Currency\Models\Currency;
 use Korobochkin\Currency\Plugin;
 
 class CurrencyTable {
@@ -18,40 +20,69 @@ class CurrencyTable {
 	public $table;
 
 	public function get_table() {
-		if( !empty( $this->parameters['currency_list'] ) && \Korobochkin\Currency\Service\Rates::is_available() ) {
+		if( !empty( $this->parameters['currency_list'] ) ) {
 
 			$this->table = new Table();
-			$this->table->set_heading(
-				array(
-					__( 'Currency', Plugin::NAME ),
-					__( 'Rate', Plugin::NAME ) . ' ' . $this->parameters['base_currency'],
-					'%'
-				)
-			);
-			$rates = get_option( \Korobochkin\Currency\Plugin::NAME . '_rates' );
+			// TODO: Остановились здесь
+
+			$this->table->set_heading( array(
+				__( 'Currency', Plugin::NAME ),
+				__( 'Rate', Plugin::NAME ) . ' ' . $this->parameters['base_currency'],
+				'%'
+			) );
 
 			foreach( $this->parameters['currency_list'] as $currency ) {
-				$rate = \Korobochkin\Currency\Service\Rates::get_rate( $currency, $this->parameters['base_currency'] );
-				$rate = number_format_i18n($rate, 2);
-				$percentage = \Korobochkin\Currency\Service\Rates::get_change_rate_percentage( $currency );
-				$percentage = number_format_i18n($percentage, 2);
 
-				if( $rate ) {
-					$flag = '';
-					if( $this->parameters['flag_size'] > 0 ) {
-						$flag = '<img src="' . \Korobochkin\Currency\Service\Rates::get_currency_flag( $currency, $this->parameters['flag_size'] ) . '">';
+				// Валюта
+				$currency_obj = new Currency( $this->parameters['base_currency'], $currency );
+
+				// Страна
+				$country_obj = new Country();
+				$country_obj->set_country_by_currency( $currency );
+
+				// Получаем текущую цену и форматируем ее
+				$output_data[] = $currency_obj->get_rate();
+				$output_data[] = $currency_obj->get_change_percentage();
+				$output_data[] = $country_obj->get_flag_url();
+
+				foreach( $output_data as $key => $output_data_single ) {
+					if( !$output_data_single ) {
+						unset($output_data[$key]);
+						continue;
 					}
-
-					$this->table->add_row(
-						esc_html( $currency ) . $flag,
-						$rate,
-						$percentage
-					);
+					else {
+						if( is_numeric( $output_data_single ) ) {
+							$output_data[$key] = number_format_i18n( $output_data[$key], 2 );
+						}
+					}
+				}
+				if( !empty( $output_data ) ) {
+					$this->table->add_row();
 				}
 			}
 			return $this->table->generate();
 		}
 
 		return '';
+	}
+
+	public function merge_defaults() {
+		$this->parameters['currency_list'] = array();
+		if( empty( $this->parameters['base_currency'] ) ) {
+			$this->parameters['base_currency'] = 'USD';
+		}
+
+	}
+
+	public function is_valid() {
+		if( empty( $this->parameters['currency_list'] ) || !is_array( $this->parameters['currency_list'] ) ) {
+			return false;
+		}
+		if( empty( $this->parameters['base_currency'] ) ) {
+			return false;
+		}
+		if( !isset( $this->parameters['flag_size'] ) ) {
+			return false;
+		}
 	}
 }
--- plugin/source/Widgets/CurrencyTable/Currency_Table.php
+++ plugin/source/Widgets/CurrencyTable/Currency_Table.php
@@ -33,52 +33,74 @@ class Currency_Table extends \WP_Widget {
 		/**
 		 * Table
 		 */
-		$table = new \Korobochkin\Currency\Service\CurrencyTable();
-		$currency_list = explode( ', ', $instance['currency_list'] );
+		if( !empty( $instance['currency_list'] ) ) {
+			$instance['currency_list'] = str_replace( ' ', '', $instance['currency_list'] );
+			$instance['currency_list'] = explode( ',', $instance['currency_list'] );
 
-		if( !empty( $currency_list ) ) {
-			$table->parameters = array(
-				'base_currency' => empty( $instance['base_currency'] ) ? 'USD' : $instance['base_currency'],
-				'currency_list' => $currency_list,
-				'flag_size' => !empty( $instance['flag_size'] ) && $instance['flag_size'] > 0 ? $instance['flag_size'] : 0
-			);
-			echo $table->get_table();
+			if( !empty( $instance['currency_list'] ) ) {
+
+				if( !empty( $instance['base_currency'] ) ) {
+
+					$table = new \Korobochkin\Currency\Service\CurrencyTable();
+					$table->parameters = array(
+						'base_currency' => $instance['base_currency'],
+						'currency_list' => $instance['currency_list'],
+						'flag_size' => !empty( $instance['flag_size'] ) ? $instance['flag_size'] : 0,
+						'table_headers' => $instance['table_headers']
+					);
+					echo $table->get_table();
+				}
+			}
 		}
 
 		echo $args['after_widget'];
 	}
 
 	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['title'] = sanitize_text_field( stripslashes( $new_instance['title'] ) );
-		$instance['base_currency'] = sanitize_text_field( stripslashes( $new_instance['base_currency'] ) );
-		$instance['currency_list'] = sanitize_text_field( stripslashes( $new_instance['currency_list'] ) );
-		$instance['flag_size'] = sanitize_text_field( stripslashes( $new_instance['flag_size'] ) );
-		return $instance;
+		$instance = $this->_merge_instance_with_default_instance( $new_instance );
+
+		// Basic stuff
+		$instance_to_save['title'] = sanitize_text_field( $instance['title'] );
+
+		$instance_to_save['base_currency'] = strtoupper( sanitize_text_field( $instance['base_currency'] ) );
+
+		// Вероятно, это надо сохранять в виде массива
+		$instance_to_save['currency_list'] = strtoupper( sanitize_text_field( $instance['currency_list'] ) );
+
+		$instance_to_save['flag_size'] = (int)sanitize_text_field( $instance['flag_size'] );
+
+		// Table headers
+		$instance_to_save['table_headers']['currencies'] = sanitize_text_field( $instance['table_headers']['currencies'] );
+		$instance_to_save['table_headers']['price'] = sanitize_text_field( $instance['table_headers']['price'] );
+		$instance_to_save['table_headers']['change'] = sanitize_text_field( $instance['table_headers']['change'] );
+
+		/**
+		 * Сохраняя лишь нужные переменные,
+		 * мы убираем возможность (дырку) запихнуть что-либо ненужное в БД
+		 */
+		return $instance_to_save;
 	}
 
 	public function form( $instance ) {
 		static $first = true;
 
 		if( !\Korobochkin\Currency\Service\Rates::is_available() ) {
+			?><p><?php _e( 'Select data provider in plugin settings.', Plugin::NAME ); ?></p><?php
 			return;
 		}
 
-		$title = sanitize_text_field( $instance['title'] );
-		$base_currency = sanitize_text_field( $instance['base_currency'] );
-		$currency_list = sanitize_text_field( $instance['currency_list'] );
-		$current_size = sanitize_text_field( $instance['flag_size'] );
+		$instance = $this->_merge_instance_with_default_instance($instance);
 		?>
 
 		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', Plugin::NAME ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>"></p>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>"></p>
 
 		<p><label for="<?php echo $this->get_field_id( 'base_currency' ); ?>"><?php _e( 'Base currency:', Plugin::NAME ); ?></label>
-		<input class="widefat plugin__currency__autocomplete" id="<?php echo $this->get_field_id( 'base_currency' ); ?>" name="<?php echo $this->get_field_name( 'base_currency' ); ?>" type="text" value="<?php echo esc_attr( $base_currency ); ?>"></p>
+		<input class="widefat plugin__currency__autocomplete" id="<?php echo $this->get_field_id( 'base_currency' ); ?>" name="<?php echo $this->get_field_name( 'base_currency' ); ?>" type="text" value="<?php echo esc_attr( $instance['base_currency'] ); ?>"></p>
 		<p class="description"><?php _e( 'The currency in which will be settled other currencies.', Plugin::NAME ); ?></p>
 
 		<p><label for="<?php echo $this->get_field_id( 'currency_list' ); ?>"><?php _e( 'Currencies list:', Plugin::NAME ); ?></label>
-		<input class="widefat plugin__currency__autocomplete" id="<?php echo $this->get_field_id( 'currency_list' ); ?>" name="<?php echo $this->get_field_name( 'currency_list' ); ?>" type="text" value="<?php echo esc_attr( $currency_list ); ?>"></p>
+		<input class="widefat plugin__currency__autocomplete" id="<?php echo $this->get_field_id( 'currency_list' ); ?>" name="<?php echo $this->get_field_name( 'currency_list' ); ?>" type="text" value="<?php echo esc_attr( $instance['currency_list'] ); ?>"></p>
 		<p class="description"><?php _e( 'The currencies which will be displayed in table. Separate by commas.', Plugin::NAME ); ?></p>
 
 		<script type="text/javascript">
@@ -103,14 +125,47 @@ class Currency_Table extends \WP_Widget {
 				printf(
 					'<option value="%s"%s>%s</option>',
 					esc_attr( $size ),
-					selected( $size, $current_size, false ),
+					selected( $size, $instance['flag_size'], false ),
 					esc_html( $size . ' px' )
 				);
 			}
 			?>
 		</select>
 		</p>
+
+		<h3><?php _e( 'Table headers', Plugin::NAME ); ?></h3>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'table_headers[currencies]' ); ?>"><?php _e( 'Currencies names col:', Plugin::NAME ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'table_headers[currencies]' ); ?>" name="<?php echo $this->get_field_name( 'table_headers[currencies]' ); ?>" type="text" value="<?php echo esc_attr( $instance['table_headers']['currencies'] ); ?>">
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'table_headers[price]' ); ?>"><?php _e( 'Price col:', Plugin::NAME ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'table_headers[price]' ); ?>" name="<?php echo $this->get_field_name( 'table_headers[price]' ); ?>" type="text" value="<?php echo esc_attr( $instance['table_headers']['price'] ); ?>">
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'table_headers[change]' ); ?>"><?php _e( 'Change col:', Plugin::NAME ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'table_headers[change]' ); ?>" name="<?php echo $this->get_field_name( 'table_headers[change]' ); ?>" type="text" value="<?php echo esc_attr( $instance['table_headers']['change'] ); ?>">
+		</p>
 		<?php
 		$first = false;
+	}
+
+	private function _merge_instance_with_default_instance( $instance ) {
+		$def_settings = array(
+			'title' => __( 'Exchange table', Plugin::NAME ),
+			'base_currency' => __( 'USD', Plugin::NAME ),
+			'currency_list' => _x( 'CAD, AUD, GBP', 'WARNING: always use commas as separator', Plugin::NAME ),
+			'flag_size' => 16,
+			'table_headers' => array(
+				'currencies' => __( 'Currencies', Plugin::NAME ),
+				'price' => __( 'Rate', Plugin::NAME ),
+				'change' => __( 'Change %', Plugin::NAME ),
+				'someting' => 'default value here!'
+			)
+		);
+		return wp_parse_args($instance, $def_settings);
 	}
 }
