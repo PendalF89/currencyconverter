7cc24d9844e4349d67a70c1f10c16fe1ae986bf0
Kolya Korobochkin <me@korobochkin.com>
2015-12-16 14:25:24 +0300

    Грант-задачи для сборки стилей из Less

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,38 @@
 
 module.exports = function(grunt) {
 
+  var configBridge = grunt.file.readJSON('bower_components/bootstrap/grunt/configBridge.json', { encoding: 'utf8' });
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // CSS
+    less: {
+      frontendWidgets: {
+        options: {
+          strictMath: true,
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: 'widgets.css.map',
+          sourceMapFilename: 'out/styles/widgets/widgets.css.map'
+        },
+        src: 'styles/widgets/main.less',
+        dest: 'out/styles/widgets/main.css'
+      }
+    },
+
+    // Vendor prefixes for CSS
+    autoprefixer: {
+      options: {
+        browsers: configBridge.config.autoprefixerBrowsers
+      },
+      frontendWidgets: {
+        options: {
+          map: true
+        },
+        src: 'out/styles/widgets/main.css'
+      }
+    },
 
     // Copy
     copy: {
@@ -20,6 +50,16 @@ module.exports = function(grunt) {
             dest: 'plugin/libs/flags/flags-iso/'
           }
         ]
+      },
+      styles: {
+        files: [
+          {
+            expand: true,
+            cwd: 'styles/',
+            src: '**',
+            dest: 'plugin/styles/'
+          }
+        ]
       }
     }
   });
@@ -28,6 +68,17 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', [
     'copy:composer',
-    'copy:libs'
+    'copy:libs',
+    'styles'
+  ]);
+
+  grunt.registerTask('styles', [
+    'stylesFrontendWidgets',
+    'copy:styles'
+  ]);
+
+  grunt.registerTask('stylesFrontendWidgets', [
+    'less:frontendWidgets',
+    'autoprefixer:frontendWidgets'
   ]);
 };
