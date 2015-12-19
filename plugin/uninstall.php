d0f7b1ea1f6323a6bd5ca12d32bd83d836797e78
Kolya Korobochkin <me@korobochkin.com>
2015-12-19 15:40:40 +0300

    Обновляем деинсталятор

--- plugin/uninstall.php
+++ plugin/uninstall.php
@@ -1,14 +1,12 @@
 <?php
 // If uninstall is not called from WordPress, exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
 // TODO: Написать деинсталятор
-
-delete_option( 'currency' );
+delete_option( \Korobochkin\Currency\Models\Settings\General::$option_name );
 delete_option( 'currency_rates' );
-delete_option( 'currency_rates_available' );
 
 //delete_transient( \Korobochkin\Currency\Plugin::NAME . '_provider_rates_' .  );
 //set_transient( Plugin::NAME . '_provider_rates_' . $info['id'], $prepare_transient, 24 * HOUR_IN_SECONDS );
