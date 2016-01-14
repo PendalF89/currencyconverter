ef5647ac5a300de1b528bb1fb2608c1dacdddd64
Kolya Korobochkin <me@korobochkin.com>
2016-01-14 16:21:43 +0300

    Удаляем transient правильно

--- plugin/uninstall.php
+++ plugin/uninstall.php
@@ -7,6 +7,6 @@ if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 delete_option( \Korobochkin\CurrencyConverter\Models\Settings\General::$option_name );
 delete_option( \Korobochkin\CurrencyConverter\Plugin::NAME . '_rates' );
 
-delete_transient( \Korobochkin\CurrencyConverter\Plugin::NAME . '_providers' );
+delete_transient( \Korobochkin\CurrencyConverter\Models\DataProviders::getInstance()->get_transient_name() );
 
 wp_clear_scheduled_hook( \Korobochkin\CurrencyConverter\Plugin::NAME . \Korobochkin\CurrencyConverter\Cron\UpdateCurrency::$action_name );
