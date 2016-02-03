baff8f180a90517d89f43096066419286b93f2f2
Kolya Korobochkin <me@korobochkin.com>
2016-02-03 15:44:47 +0300

    change versions

--- plugin/plugin.php
+++ plugin/plugin.php
@@ -6,7 +6,7 @@ Plugin URI: https://wordpress.org/plugins/currencyconverter/
 Description: Currency widgets for any needs.
 Author: LB company
 Author URI: http://exchangerate.guru
-Version: 0.3.0
+Version: 0.4.0
 Text Domain: currencyconverter
 Domain Path: /languages/
 Requires at least: 4.0.0
