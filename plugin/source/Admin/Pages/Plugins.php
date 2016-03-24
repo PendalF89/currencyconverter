1fcc60817b30b1728c0897d394a78405a678a885
Kolya Korobochkin <me@korobochkin.com>
2016-03-24 19:48:28 +0300

    improve plugin action links

--- plugin/source/Admin/Pages/Plugins.php
+++ plugin/source/Admin/Pages/Plugins.php
@@ -4,9 +4,16 @@ namespace Korobochkin\CurrencyConverter\Admin\Pages;
 use Korobochkin\CurrencyConverter\Plugin;
 
 class Plugins {
+	
 	public static function add_action_links( $links ) {
-		return array_merge( $links, array(
-			'<a href="' . \Korobochkin\CurrencyConverter\Admin\Settings\General\Pages\General\Page::get_url() . '">' . __( 'Settings', Plugin::NAME ) . '</a>'
-		) );
+		$add = array(
+			sprintf(
+				'<a href="%1$s">%2$s</a>',
+				esc_url( \Korobochkin\CurrencyConverter\Admin\Settings\General\Pages\General\Page::get_url() ),
+				esc_html( __( 'Settings', Plugin::NAME ) )
+			)
+		);
+
+		return array_merge( $links, $add );
 	}
 }
